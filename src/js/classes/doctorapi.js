389de989363ca4cf2a870abb180e722b51ed59e5
wchamberlain89 <wchamberlain89@gmail.com>
2019-03-22 09:17:37 -0700

    it will have doctorAPi class

---
+++ src/js/classes/doctorapi.js
@@ -0,0 +1,19 @@
+export class DoctorApi {
+	get(query) {
+    return new Promise(function(resolveFunction, rejectFunction) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${$query}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolveFunction(request.response);
+        } else {
+          rejectFunction(Error(request.statusText));
+        }
+      }
+
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
