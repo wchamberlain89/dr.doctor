4c39703e47fcc50ccc3ec7d6ce8a0c4e81df52e8
wchamberlain89 <wchamberlain89@gmail.com>
2019-03-22 09:31:24 -0700

    Webpack updated with dotenv, will grab search term and create promise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import $ from 'jquery';
 import './scss/styles.scss';
+import {DoctorApi} from './js/classes/doctorapi.js';
 
 $(document).ready(function(){
+	const doctorApi = new DoctorApi();
+
   $("form").on("submit",() => {
-  	console.log("hello");
+  	const query = $("#search").val();
+  	console.log(query);
+  	console.log(process.env.exports.apiKey);
+  	// const promise = doctorApi.get(query);
   	event.preventDefault();
   });
 });
