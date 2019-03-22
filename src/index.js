b42d3586c8894b6ca6ce6a44f702e66d8ad23a63
wchamberlain89 <wchamberlain89@gmail.com>
2019-03-22 13:45:55 -0700

    it will parse object of parameters into a string

--- src/index.js
+++ src/index.js
@@ -2,26 +2,34 @@ import $ from 'jquery';
 import './scss/styles.scss';
 import {DrApi} from './js/classes/drApi.js';
 import {Geocode} from './js/classes/geocode.js';
-import {apiController} from './js/functions/promise.js';
+import {setUrl} from './js/functions/promise.js';
+
+function parseParams(parameters) {
+  return Object.keys(parameters).reduce((acc, key, index) => {
+    return acc += key + "=" + parameters[key] + "&";
+  }, "?");
+}
+
 
 $(document).ready(function(){
 
-	const apiControl = new apiController;
+  const drApi = new DrApi();
+  drApi.getDoctors = setUrl(drApi.baseURL + 'doctors');
 
-	const drApi = new DrApi();
-  drApi.getDoctors = apiControl.setGet(drApi.baseURL + 'doctors');
-	
-	const geocodeApi = new Geocode();
-	geocodeApi.getByGeocode = apiControl.setGet(geocodeApi.baseURL + 'geocode');
-
-	console.log(geocodeApi.getByGeocode);
+  const geocodeApi = new Geocode();
+  geocodeApi.getByGeocode = setUrl(geocodeApi.baseURL + 'geocode');
 
   $("form").on("submit",(event) => {
   	event.preventDefault();
   	
   	const query = $("#search").val();
   	
-  	geocodeApi.getByGeocode()
-		
+    const geocodeParams = parseParams({city : 'portland', state: 'or'});
+
+    geocodeApi.getByGeocode(geocodeParams)
+    .then((res) => {
+     console.log(res);
+   });
+
   });
 });
--- src/js/functions/promise.js
+++ src/js/functions/promise.js
@@ -1,25 +1,20 @@
-export class apiController {
-  setGet(url) {
-    return function (parameters)  {
-      return new Promise(function(resolveFunction, rejectFunction) {
-        let request = new XMLHttpRequest();
+export function setUrl(url) {
+  return function (parameters)  {
 
-        parameters.
-
-        request.onload = function() {
-          if (this.status === 200) {
-            resolveFunction(request.response);
-          } else {
-            rejectFunction(Error(request.statusText));
-          }
+    return new Promise(function(resolveFunction, rejectFunction) {
+      let request = new XMLHttpRequest();
+      console.log(url)
+      url = url + parameters;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolveFunction(request.response);
+        } else {
+          rejectFunction(Error(request.statusText));
         }
+      }
 
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
-  }
-  parseParams(parameters) {
-    
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
