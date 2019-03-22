0d1ed141646bcb89b2952c530577735f70dbfd25
wchamberlain89 <wchamberlain89@gmail.com>
2019-03-22 09:20:55 -0700

    it will submit form

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import $ from 'jquery';
 import './scss/styles.scss';
 
 $(document).ready(function(){
-  
+  $("form").on("submit",() => {
+  	console.log("hello");
+  	event.preventDefault();
+  });
 });
--- src/js/classes/doctorapi.js
+++ src/js/classes/doctorapi.js
@@ -2,7 +2,7 @@ export class DoctorApi {
 	get(query) {
     return new Promise(function(resolveFunction, rejectFunction) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${$query}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${$query}user_key=${process.env.exports.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
